bd7958b154423e19d186b9d0136a87c7d2f305b3
jcoop32 <jcooper11@hawk.iit.edu>
2025-05-15 12:35:07 -0500

    fix text file

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
+
 class TestApp(App):
     def build(self):
-        return Label(text='Hello Pi!')
+        return Label(text="Hello Pi!")
 
-TestApp().run()
 
+if __name__ == "__main__":
+    TestApp().run()
